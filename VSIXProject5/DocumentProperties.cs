ccf2d8582c58b2ec415d4e7e32531d31b65b71ff
Emil <exidge@gmail.com>
2017-09-17 17:16:09 +0200

    Changed logic for searching in xml documents. Changed logic for navigating to search result.

---
+++ VSIXProject5/DocumentProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSIXProject5
+{
+    public class DocumentProperties
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string RelativePath { get; set; }
+    }
+}
