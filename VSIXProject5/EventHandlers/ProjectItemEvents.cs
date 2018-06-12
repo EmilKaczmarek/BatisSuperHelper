b54f219a96e52abc052a1913d242818aee1a53a7
EmilKaczmarek <emil.kaczmarek91@gmail.com>
2018-06-12 19:14:31 +0200

    Goto Command ultimate cleanup: Moved logic that is related to whole VS Instance to GotoPackage. Moved action to new file. Created base class for command actions.

---
+++ VSIXProject5/EventHandlers/ProjectItemEvents.cs
@@ -0,0 +1,36 @@
+﻿using EnvDTE;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSIXProject5.Indexers;
+
+namespace VSIXProject5.EventHandlers
+{
+    public class ProjectItemEventsEx
+    {
+        public void ItemRenamed(ProjectItem ProjectItem, string OldName)
+        {
+            Indexer.RenameStatmentsFile(OldName, ProjectItem.Name);
+        }
+
+        public void ItemRemoved(ProjectItem ProjectItem)
+        {
+            string fileName = ProjectItem.Name;
+            Indexer.RemoveStatmentsForFile(fileName, false);
+        }
+
+        public void ItemAdded(ProjectItem ProjectItem)
+        {
+            string projectItemExtension = Path.GetExtension(ProjectItem.Name);
+            if (projectItemExtension == ".xml")
+            {
+                XmlIndexer indexerInstance = new XmlIndexer();
+                var xmlIndexer = indexerInstance.BuildUsingFilePath(ProjectItem.FileNames[0]);
+                Indexer.Build(xmlIndexer);
+            }
+        }
+    }
+}
