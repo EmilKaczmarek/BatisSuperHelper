3684a21b2b42127abadb9ddd6c1541de8546ad9c
EmilKaczmarek <emil.kaczmarek91@gmail.com>
2018-06-17 11:32:22 +0200

    Added stub for rename command.

---
+++ VSIXProject5/Actions/QueryRenameActions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSIXProject5.Actions.Abstracts;
+
+namespace VSIXProject5.Actions
+{
+    public class QueryRenameActions : BaseActions
+    {
+        public override void BeforeQuery(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Change(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MenuItemCallback(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
