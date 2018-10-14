83e86bb8fc7261e0d34c8f06486a310d08a3ce9e
Emil <exidge@gmail.com>
2018-10-14 19:00:49 +0200

    Reworked Result Window. Changed Action to integrate with new Result Window.

--- VSIXProject5/Windows/ResultWindow/ResultWindowControl.xaml.cs
+++ VSIXProject5/Windows/ResultWindow/ResultWindowControl.xaml.cs
@@ -4,51 +4,67 @@
     using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Media;
     using VSIXProject5.Indexers.Models;
     using VSIXProject5.VSIntegration.Navigation;
+    using VSIXProject5.Windows.ResultWindow.ViewModel;
+
 
     /// <summary>
     /// Interaction logic for ResultWindowControl.
     /// </summary>
     public partial class ResultWindowControl : UserControl
     {
+        private static double _lastColumnInitialWidth;
         /// <summary>
         /// Initializes a new instance of the <see cref="ResultWindowControl"/> class.
         /// </summary>
         public ResultWindowControl()
         {
             this.InitializeComponent();
-            this.MouseDoubleClick += ResultWindowControl_MouseDoubleClick;
 
+            _lastColumnInitialWidth = (listView.View as GridView).Columns[(listView.View as GridView).Columns.Count - 1].Width;
         }
 
-        private void ResultWindowControl_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        public void ShowResults(List<ResultWindowViewModel> resultWindowsViewModels)
         {
-            DataGridRow row = sender as DataGridRow;
-            var currentItem = dataGrid.CurrentItem as BaseIndexerValue;
-            //exception on opening visual studio
-            DocumentNavigationInstance.instance.OpenDocumentAndHighlightLine(currentItem.QueryFilePath, currentItem.QueryLineNumber);
+            this.listView.ItemsSource = resultWindowsViewModels;
         }
 
-        /// <summary>
-        /// Handles click on the button by displaying a message box.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
-        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
-        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void listView_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            MessageBox.Show(
-                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
-                "ResultWindow");
+            ResizeLastColumn(sender as ListView);
         }
-        public void ShowResults<T>(List<T> results) where T: BaseIndexerValue
+
+        private void listView_Loaded(object sender, RoutedEventArgs e)
         {
-            dataGrid.ItemsSource = null;
-            dataGrid.ItemsSource = results;
+            ResizeLastColumn(sender as ListView);
         }
 
+        private void ResizeLastColumn(ListView listView)
+        {
+            var gridView = listView.View as GridView;
+            double columnsWidthWithoutLast = 0;
+            for(int i = 0; i < gridView.Columns.Count-1; i++)
+            {
+                columnsWidthWithoutLast += gridView.Columns[i].ActualWidth;
+            }
 
+            var lastColumn = gridView.Columns[gridView.Columns.Count-1];
+
+            lastColumn.Width = _lastColumnInitialWidth;
+            var lastColumnTargetWidth = listView.ActualWidth - columnsWidthWithoutLast;
+            if(lastColumn.Width < lastColumnTargetWidth)
+            {
+                lastColumn.Width = lastColumnTargetWidth;
+            }
+            
+        }
+
+        private void listView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var selectedItem = (sender as ListView).SelectedItem as ResultWindowViewModel;
+            DocumentNavigationInstance.instance.OpenDocumentAndHighlightLine(selectedItem.FilePath, selectedItem.Line);
+        }
     }
 }
