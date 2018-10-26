6cd7f3e5e648cd477b4497964e916654bf37da8e
EmilKaczmarek <emil.kaczmarek91@gmail.com>
2018-10-26 20:29:30 +0200

    Changed logic for handling text change. Package now alows MEF exporting. Version up.

---
+++ VSIXProject5/VSIntegration/DocumentChanges/Actions/IOnFileContentChange.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSIXProject5.VSIntegration.DocumentChanges.Actions
+{
+    public interface IOnFileContentChange
+    {
+        void HandleChange(IWpfTextView textView);
+    }
+}
---
+++ VSIXProject5/VSIntegration/DocumentChanges/DocumentChangesTextViewCreationListener.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Reactive.Linq;
+using VSIXProject5.HelpersAndExtensions.VisualStudio;
+using VSIXProject5.VSIntegration.DocumentChanges.Actions;
+using Microsoft.VisualStudio.Text;
+using System.ComponentModel.Composition;
+
+namespace VSIXProject5.VSIntegration.DocumentChanges
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class DocumentChangesTextViewCreationListener : IWpfTextViewCreationListener
+    {
+        [Export(typeof(AdornmentLayerDefinition))]
+        [Name("TextAdornmentExample")]
+        [Order(After = PredefinedAdornmentLayers.Selection, Before = PredefinedAdornmentLayers.Text)]
+        private AdornmentLayerDefinition editorAdornmentLayer;
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            var contentType = textView.Caret.Position.BufferPosition.Snapshot.GetContentTypeName();
+            IOnFileContentChange fileAction = contentType == "CSharp" ? new  CSharpFileContentOnChange() : (IOnFileContentChange)new XmlFileContentOnChange();
+            Observable.FromEventPattern<TextContentChangedEventArgs>(textView.TextBuffer, "Changed")
+               //.Select(e => e.EventArgs.Changes)
+               .DistinctUntilChanged()
+               .Throttle(TimeSpan.FromMilliseconds(500))
+               .Subscribe(e =>
+               {
+                   fileAction.HandleChange(textView);
+               });
+        }
+    }
+}
