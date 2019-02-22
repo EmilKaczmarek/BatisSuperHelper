657e0768b501c616a50abbddf333f008a9e84ab6
Emil <exidge@gmail.com>
2019-02-22 22:03:34 +0100

    Unused query validator: -Add missing CreationListener. -Removed unused Import.

---
+++ VSIXProject5/VSIntegration/BatisFilesTextViewIntegration/XmlTextViewCreationListener.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBatisSuperHelper.VSIntegration.BatisFilesTextViewIntegration
+{
+    [Export(typeof(IVsTextViewCreationListener))]
+    [ContentType("XML")]
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
+    public class XmlTextViewCreationListener : IVsTextViewCreationListener
+    {
+        [Import]
+        IVsEditorAdaptersFactoryService AdaptersFactory = null;
+
+        [Import]
+        internal SVsServiceProvider ServiceProvider = null;
+
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            IWpfTextView view = AdaptersFactory.GetWpfTextView(textViewAdapter);
+
+            view.TextBuffer.Properties.GetOrCreateSingletonProperty(() => view);
+            view.TextBuffer.Properties.GetOrCreateSingletonProperty(() => new ErrorListProvider(ServiceProvider));
+        }
+    }
+}
